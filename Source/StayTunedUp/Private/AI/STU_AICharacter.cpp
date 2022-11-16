b18e5c39892fda379aa70eb1e578d05854b3426a
intvoker <intvoker@gmail.com>
2022-11-16 01:45:58 -0800

    Smooth AICharacter Rotation

--- Source/StayTunedUp/Private/AI/STU_AICharacter.cpp
+++ Source/StayTunedUp/Private/AI/STU_AICharacter.cpp
@@ -4,10 +4,17 @@
 #include "AI/STU_AICharacter.h"
 
 #include "AI/STU_AIController.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 ASTU_AICharacter::ASTU_AICharacter(const FObjectInitializer& ObjectInitializer):
 	Super(ObjectInitializer)
 {
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 	AIControllerClass = ASTU_AIController::StaticClass();
+
+	bUseControllerRotationYaw = false;
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->bUseControllerDesiredRotation = true;
+	}
 }
